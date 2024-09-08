53fdee72e765a0b2bebbceb98dbd6f92a661137d
Gasper Vrhovsek <gasper.vrhovsek@gmail.com>
2024-09-08 15:19:30 +0200

    Initial commit

---
+++ src/main/java/si/gaspervrhovsek/Main.java
@@ -0,0 +1,12 @@
+package si.gaspervrhovsek;
+
+import io.quarkus.runtime.QuarkusApplication;
+import io.quarkus.runtime.annotations.QuarkusMain;
+
+@QuarkusMain
+public class Main implements QuarkusApplication {
+    @Override
+    public int run(final String... args) throws Exception {
+        return 0;
+    }
+}
